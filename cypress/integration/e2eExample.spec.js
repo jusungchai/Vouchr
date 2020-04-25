f1edc9f491c234defd3e95523e8f84b96ca1c650
JS Chai <chunsaa@gmail.com>
2020-04-25 00:18:20 -0400

    refactor code by separating each steps to its own it block for clarity

--- cypress/integration/e2eExample.spec.js
+++ cypress/integration/e2eExample.spec.js
@@ -4,25 +4,30 @@ describe("Demo E2E", () => {
   //time to wait (change this value depending on DUT spec)
   const waitTime = 3000;
 
-  it("Should select template from Birthday category -> Add Note > Add Youtube > Add Interview (search on tenor) GIF > Remove GIF > Preview > Send", () => {
-    //visit root page
+  it("Should visit root page", () => {
     cy.visit("/")
+  })
 
-    //wait 3s for page to fully render (this value is bigger)
-    //find the carousel and click to render egreeting
+  it("Should wait for iframe to load and click the egreetings banner", () => {
     updatedDOM('iframe[title=PersonalizationCarousel]', waitTime)
       .find('.App_content__3La4L')
       .first('.PersonalizationCarousel_CellContent__3srcj > .PersonalizationCarousel_CellContent__3srcj').click()
+  })
 
+  it("Should select Birthday Category template", () => {
     selectCategory('Big Birthday')
+  })
 
+  it("Should add Note", () => {
     addMedia('Note')
 
     //Type birthday message
     updatedDOM('iframe[title=DiscoverApp]')
       .type('Happy Birthday Suresh and Vishwa!')
       .contains('DONE').click()
+  })
 
+  it("Should add Youtube", () => {
     addMedia('YouTube')
 
     //Search for video
@@ -34,7 +39,9 @@ describe("Demo E2E", () => {
     updatedDOM('iframe[title=DiscoverApp]')
       .find('.Youtube-module_SearchResults__jDFiK > button')
       .eq(0).click()
+  })
 
+  it("Should add Gif", () => {
     addMedia('Gif')
 
     //Click on input field (disabled at default) and search
@@ -48,35 +55,54 @@ describe("Demo E2E", () => {
       .find('.ResultsPage-module_column__3zcIx > img')
       .eq(0).click()
 
-    //Add selected gif to gift
     updatedDOM('iframe[title=DiscoverApp]')
       .contains('ADD TO GIFT').click()
+  })
 
-    //Remove added gif
+  it("Should remove Gif", () => {
     updatedDOM('iframe[title=DiscoverApp]')
-      .find('#vouchrAppRoot > div > div:nth-child(1) > div.App_content__3La4L > div.Preview_PreviewContainer__39hcn > div > div.Preview_ContentContainer__BjrSF > div:nth-child(10) > div > div.Media_Controls__1zHMA.flex.justify-between.w-full.absolute > div.Media_ControlsRight__3yZfs > button').click()
+      .find(`#vouchrAppRoot > div > div:nth-child(1) > div.App_content__3La4L > 
+      div.Preview_PreviewContainer__39hcn > div > div.Preview_ContentContainer__BjrSF > 
+      div:nth-child(10) > div > div.Media_Controls__1zHMA.flex.justify-between.w-full.absolute > 
+      div.Media_ControlsRight__3yZfs > button`).click()
+  })
 
-    //Tools > Preview > Next > Continue
+  it("Should preview egreetings", () => {
     updatedDOM('iframe[title=DiscoverApp]')
       .contains('Tools').click()
     updatedDOM('iframe[title=DiscoverApp]')
       .contains('Preview').click()
+  })
+
+  it("Should submit egreetings", () => {
     updatedDOM('iframe[title=DiscoverApp]')
       .contains('Next').click({ force: true })
     updatedDOM('iframe[title=DiscoverApp]')
       .contains('Continue').click()
+  })
 
+  it("Should show that egreetings is added", () => {
     updatedDOM('iframe[title=PersonalizationCarousel]', waitTime)
       .contains('Your Gift is Ready!')
+  })
 
+  it("Should submit the money transfer", () => {
     cy.get('.my-4').contains('Send').click({ force: true })
+  })
 
+  it("Should show and goto gift link", () => {
     cy.get('.card-link').contains('Gift Link').should('have.attr', 'href')
       .then(link => {
         cy.visit(link)
-        cy.contains('OPEN NOW').click()
-        cy.contains('Happy Birthday Suresh and Vishwa!')
-        cy.get('.VideoPlayer-module_VideoPlayerContainer__3PEPY')
       })
-  });
+  })
+
+  it("Should open egreetings", () => {
+    cy.contains('OPEN NOW').click()
+  })
+
+  it("Should validate that note and youtube are added", () => {
+    cy.contains('Happy Birthday Suresh and Vishwa!')
+    cy.get('.VideoPlayer-module_VideoPlayerContainer__3PEPY')
+  })
 });
